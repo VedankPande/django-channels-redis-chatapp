cbb7da508381903108c982646c46772caef6a0e8
Vedank Pande <vedank.pande@gmail.com>
2023-09-13 15:00:21 +0530

    minor changes

--- chat/urls.py
+++ chat/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path,include
+from django.urls import path
 from .views import ChatView
 urlpatterns = [
     path('chat/start/<str:target>', ChatView.as_view(), name="chat"),
